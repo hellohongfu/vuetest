302c563b7bd6412bf370067b8563b2654d9282e7
Hongfu Wang <hongwan@microsoft.com>
2020-05-16 09:35:47 +0800

    'complate'

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,14 @@
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App" data=""  />
   <test></test>
+  <ifelse msg="测试IF ELSE"></ifelse>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 import test from './components/test.vue'
+import ifelse from './components/ifelse.vue'
 
 export default {
   name: 'App',
@@ -16,6 +18,7 @@ export default {
     HelloWorld
     
     ,test
+    ,ifelse
   }
 }
 </script>
---
+++ src/components/ifelse.vue
@@ -0,0 +1,63 @@
+<template>
+
+<div>
+         IFLSE 测试页面{{message}}
+         <br>
+         isTrue:{{isTrue}}
+          <br>
+        <a @click="doSomething">doSomtthing</a>
+        <div>
+            通过点击doSomething判断是否显示下面的方框
+            <p  v-if="dis==0"  style=" display:block; background-color:red; color:white">
+                v-if方框 {{dis}}
+            </p>
+             <p  v-else-if="dis==1"  style=" display:block; background-color:green; color:white">
+                 v-else方框 {{dis}}
+            </p>
+             <p  v-else  style=" display:block; background-color:black; color:white">
+                 v-else方框 {{dis}}
+            </p>
+
+             <p  v-show="isTrue"  style=" display:block; background-color:black; color:white">
+                 v-show {{isTrue}}
+            </p>
+        </div>
+
+</div>
+
+</template>
+<script>
+export default{
+
+    name:"条件循环",
+    props:{msg:String}
+    ,data:function(){
+        return {
+
+            isTrue:true
+            ,dis:0
+        }
+    }
+    ,methods:{
+        doSomething:function(){
+            this.isTrue=!this.isTrue;
+            if(this.dis==0){
+            this.dis=1;
+
+            }else  if(this.dis==1) {
+            this.dis=2;
+
+            }else
+            {
+
+            this.dis=0;
+
+            }
+        }
+    }
+
+}
+</script>
+<style>
+
+</style>
